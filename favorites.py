7378f57780eb69ec26ad091fcdda00c6797b3285
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-27 13:53:09 -0400

    Updated from Brython Server: 9/27/2017 1:53:08 PM

--- favorites.py
+++ favorites.py
@@ -1,1 +1,10 @@
+#Andrew Parker
+#9/27/17
+#loopDemo.py - how to use loops
+
+fword = input('What is your favorite word? ')
+fnum = int(input('what is your favorite number? '))
+for i in range(0,fnum):
+    print(fword)
+    
 
