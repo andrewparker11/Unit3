a3495eedca664182226b139e132801aaa3708bb0
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-27 13:58:24 -0400

    Updated from Brython Server: 9/27/2017 1:58:24 PM

--- fives.py
+++ fives.py
@@ -1,1 +1,8 @@
+#Andrew Parker
+#9/27/17
+#fives.py - Prints out multiples of 5 up a certain number
 
+fword = input('What is your favorite word? ')
+fnum = int(input('what is your favorite number? '))
+for i in range(0,fnum):
+    print(fword)
