2959a59bbc26a8086813204a58beef5b2b00fe76
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-27 13:28:38 -0400

    Updated from Brython Server: 9/27/2017 1:28:37 PM

--- loopDemo.py
+++ loopDemo.py
@@ -3,5 +3,10 @@
 #loopDemo.py - how to use loops
 
 #print I love computer science 5 imes
-for i in range(1,5):
-    print('I <3 Computer Science')
+for i in range(0,5):
+    print('I <3 Computer Science')"""
+    
+#print the numbers from 1 to 20
+for i in range(1,21):
+    print(i)
+    
