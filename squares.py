443bfc5192dcda791b571dfc5a144405b1fbef80
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-28 14:37:40 -0400

    Updated from Brython Server: 9/28/2017 2:37:39 PM

--- squares.py
+++ squares.py
@@ -2,7 +2,7 @@
 #9/28/17
 #squares.py - Gives certian number of square numbers starting from 1
 
-num = int(input('Enter a number ')
+num = int(input('Enter a number '))
 i = 1
 while i<=num:
     print(i, 'Missisipi')
