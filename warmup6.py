ebce9773603c92193a4c0893056fae688485cf74
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-28 14:17:33 -0400

    Updated from Brython Server: 9/28/2017 2:17:32 PM

--- warmup6.py
+++ warmup6.py
@@ -1,1 +1,11 @@
+#Andrew Parker
+#9/28/17
+#warmup6.py - Count missisipi to 10
 
+#copy cat
+while True:
+    text = input('Type something: ')
+    print(text)
+    if text == 'something':
+        break
+print('You win! ')
