8ead86bbf5a8676d7a335fd20eeaa7f2b221285c
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-28 14:23:21 -0400

    Updated from Brython Server: 9/28/2017 2:23:20 PM

--- warmup6.py
+++ warmup6.py
@@ -2,10 +2,12 @@
 #9/28/17
 #warmup6.py - Count missisipi to 10
 
-#copy cat
-while True:
-    text = input('Type something: ')
-    print(text)
-    if text == 'something':
-        break
-print('You win! ')
+
+#For loop version
+i = 1
+for i in range(1,11):
+    print(i, 'Missisipi')
+    
+#For While loop version
+
+
