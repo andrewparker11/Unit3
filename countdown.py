e47b63d061a83272c889da5f66af27b03c52d72d
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-28 14:45:37 -0400

    Updated from Brython Server: 9/28/2017 2:45:34 PM

--- countdown.py
+++ countdown.py
@@ -2,10 +2,13 @@
 #9/28/17
 #countdown.py - Counts down to BOOM
 
-i = int(input('Enter a number '))
+"""i = int(input('Enter a number '))"""
+
+i=5
 while i>0:
     print(i)
     i = i -1
-when i=0:
+if i == 0:
     print('BOOM! ')
+    
 
