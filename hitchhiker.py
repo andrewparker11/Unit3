23b878d0057704738a9fc0bce9d9cb3ef002b6b9
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-29 14:55:01 -0400

    Updated from Brython Server: 9/29/2017 2:55:01 PM

--- hitchhiker.py
+++ hitchhiker.py
@@ -1,1 +1,11 @@
+#Andrew Parker
+#9/29/17
+#hitchhiker.py - Ask or quit
 
+#copy cat
+while True:
+    text = input('Type something: ')
+    print(text)
+    if text == 'something':
+        break
+print('You win! ')
