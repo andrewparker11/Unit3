c37db6473ba0c738bd2ac672c547669ea3542769
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-09-29 14:24:07 -0400

    Updated from Brython Server: 9/29/2017 2:24:07 PM

--- warmup7.py
+++ warmup7.py
@@ -1,1 +1,16 @@
+#Andrew Parker
+#9/28/17
+#warmup6.py - Count missisipi to 10
 
+
+#For loop version
+"""
+for i in range(1,11):
+    print(i, 'Missisipi')"""
+    
+    
+#For While loop version
+i = 1
+while i<=10:
+    print(i, 'Missisipi')
+    i = i + 1
