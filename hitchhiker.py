b78fba4c12ec82ef6db31056daafa7c1915a585c
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-02 14:38:07 -0400

    Updated from Brython Server: 10/2/2017 2:38:07 PM

--- hitchhiker.py
+++ hitchhiker.py
@@ -2,10 +2,8 @@
 #9/29/17
 #hitchhiker.py - Ask or quit
 
-#copy cat
 while True:
-    text = input('Type something: ')
-    print(text)
-    if text == 'something':
+    text = input('Ask a question or press quit: ')
+    print(42)
+    if text == 'quit':
         break
-print('You win! ')
