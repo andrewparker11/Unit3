d4eae43f292cf2948af664d0da8c9bdb4361c880
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-02 14:17:06 -0400

    Updated from Brython Server: 10/2/2017 2:17:06 PM

--- warmup8.py
+++ warmup8.py
@@ -1,1 +1,11 @@
+#Andrew Parker
+#10/2/17
+#warmup8.py - find the sum of all positive intergers less than 100,00 that are divisible by 3, 10 and 17
 
+
+word = input('Enter a word: ')
+
+for i<100,00 and i%3==0 and i%10 == 0 and i%17
+
+for ch in word:
+    print(ch+'!')
