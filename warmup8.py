ffffe19bbffe9d070eb7c08800744dfc89522aa2
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-02 14:30:14 -0400

    Updated from Brython Server: 10/2/2017 2:30:14 PM

--- warmup8.py
+++ warmup8.py
@@ -2,7 +2,8 @@
 #10/2/17
 #warmup8.py - find the sum of all positive intergers less than 100,00 that are divisible by 3, 10 and 17
 
-
+total = 0
 for i in range(0,100000):
     if i%3==0 and i%10 == 0 and i%17 == 0:
-      print(i)
+      total = total + i  
+print(total)
