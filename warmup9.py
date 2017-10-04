1207b7f72cbde954d7f431a4cc91f4dfc36c5a68
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-04 13:41:40 -0400

    Updated from Brython Server: 10/4/2017 1:41:40 PM

--- warmup9.py
+++ warmup9.py
@@ -1,1 +1,19 @@
+#Andrew Parker
+#10/4/17
+#warmup9.py - asks for a word, Prints it with capitalized vowels
 
+from random import randint
+
+numCorrect = 0
+while numCorrect < 5:
+    num1 = randint(-10,10)
+    num2 = randint(-10,10)
+    question = 'What is ' + str(num1) + ' + ' + str(num2) + '?'
+    answer = int(input(question))
+    if num1 + num2 == answer:
+        print('Correct!')
+        numCorrect += 1
+    else:
+        print('The answer was', num1+num2)
+        
+print('Congradulations! You Win! ')
