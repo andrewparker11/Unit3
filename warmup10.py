484eebad71122559f6091c868b50d5f115a6396a
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-05 14:42:03 -0400

    Updated from Brython Server: 10/5/2017 2:42:02 PM

--- warmup10.py
+++ warmup10.py
@@ -5,11 +5,26 @@
 from ggame import * 
 
 red = Color(0xFF0000,1)
+black = Color(0x000000,1)
+yellow = Color(0xFFFF00,1)
 
+
+blackOutline = LineStyle(1,black) #pixels, color
+redRectangle = RectangleAsset(60,10,blackOutline,red) #width, height, outline, fill
+blackRectangle = RectangleAsset(60,10,blackOutline,black)
+yellowRectangle = RectangleAsset(60,10,blackOutline,yellow)
 dot = CircleAsset(20,LineStyle(1,red),red)
 
 for j in range(10):
     for i in range(20):
-        Sprite(dot,(20 + 50*i,20 + 50*j))
+        Sprite(redRectangle,(20 + 50*i, 20+50*j))
+        Sprite(yellowRectangle,(20 + 50*i, 20+50*j))
+        Sprite(blackRectangle,(20 + 50*i, 20+50*j)) 
+
 App().run()
+
+
+#Sprite(dot,(20 + 50*i,20 + 50*j))
+
+
 
