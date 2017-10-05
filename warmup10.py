a4ae7e606b8521e377bdc83ec3865ec48943c0a7
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-10-05 14:36:35 -0400

    Updated from Brython Server: 10/5/2017 2:36:34 PM

--- warmup10.py
+++ warmup10.py
@@ -1,1 +1,15 @@
+#Andrew Parker
+#10/5/17
+#dotsDemo.py - Making some dots
+
+from ggame import * 
+
+red = Color(0xFF0000,1)
+
+dot = CircleAsset(20,LineStyle(1,red),red)
+
+for j in range(10):
+    for i in range(20):
+        Sprite(dot,(20 + 50*i,20 + 50*j))
+App().run()
 
